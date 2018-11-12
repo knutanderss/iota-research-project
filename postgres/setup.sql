e8fbaa69b3e7221f61dd98b271b31e8614ecc807
Steffen Sande <steffen.sande94@gmail.com>
2018-11-12 13:45:35 +0100

    Endret syntax til setup script til postgres.

--- postgres/setup.sql
+++ postgres/setup.sql
@@ -1,17 +1,17 @@
 -- Not tested yet (remove this line when tested)
 
 create table users (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(30) NOT NULL,
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(30) UNIQUE NOT NULL,
   pw VARCHAR(200) NOT NULL,
-  super INT(1) UNSIGNED NOT NULL
+  super smallint NOT NULL
 );
 
 create table acls (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   username VARCHAR(30) NOT NULL,
   topic VARCHAR(100) NOT NULL,
-  rw INT(1) NOT NULL 
+  rw smallint NOT NULL 
 );
 
 -- password: password
