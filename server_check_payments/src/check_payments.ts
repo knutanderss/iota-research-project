32494ea4eb56ef453d2d8e7f0c0e35a9d40da1ac
Knut Anders Stokke <knutandersstokke@gmail.com>
2018-11-26 11:08:38 +0100

    Remove accidental nested for loop

--- server_check_payments/src/check_payments.ts
+++ server_check_payments/src/check_payments.ts
@@ -41,29 +41,36 @@ const ACCESS_PRICE = 2;
 const HOURS = 6;
 
 // The IOTA address where funds will be deposited
-const DEPOSIT_ADDRESS = "NRHIUJL9NGMCRYT9BYRLAHAPBVIVIUIPIGWRQKTIBPNRPOOTMEGJHXBASFILYGNQIJOLGGPJMSJMNFWAXSCZVOCCSD";
+const DEPOSIT_ADDRESS =
+    "NRHIUJL9NGMCRYT9BYRLAHAPBVIVIUIPIGWRQKTIBPNRPOOTMEGJHXBASFILYGNQIJOLGGPJMSJMNFWAXSCZVOCCSD";
 
 function promisify<Arg1, Result>(
-    fn: (arg1: Arg1, cb: (err: Error, result: Result) => void) => void): (arg1: Arg1) => Promise<Result> {
-    return (arg1) => new Promise<Result>(
-        (resolve, reject) => fn(arg1, (err, result) => err ? reject(err) : resolve(result)),
-    );
+    fn: (arg1: Arg1, cb: (err: Error, result: Result) => void) => void,
+): (arg1: Arg1) => Promise<Result> {
+    return (arg1) =>
+        new Promise<Result>((resolve, reject) =>
+            fn(arg1, (err, result) => (err ? reject(err) : resolve(result))),
+        );
 }
 
 const getDeposits = (depositAddress: string) =>
     new Promise<ITransactionObject[]>((resolve, reject) =>
         iota.api.findTransactionObjects(
             { addresses: [depositAddress] },
-            (err, result) => err ? reject(err) : resolve(result),
-        ));
+            (err, result) => (err ? reject(err) : resolve(result)),
+        ),
+    );
 
 const getLatestInclusion = (hashes: string[]) =>
     new Promise<boolean[]>((resolve, reject) =>
-        iota.api.getLatestInclusion(hashes, (err, result) => err ? reject(err) : resolve(result)));
+        iota.api.getLatestInclusion(hashes, (err, result) =>
+            err ? reject(err) : resolve(result),
+        ),
+    );
 
 const isValidTransaction = (t: ITransactionObject): boolean =>
-    t.value >= ACCESS_PRICE
-    && moment(t.timestamp * 1000).isAfter(moment().subtract(HOURS, "hour"));
+    t.value >= ACCESS_PRICE &&
+    moment(t.timestamp * 1000).isAfter(moment().subtract(HOURS, "hour"));
 
 const extractAndValidateMessage = async (transaction: ITransactionObject) => {
     // message should be in the form of: '{"username": "{username}"}'
@@ -76,7 +83,10 @@ const extractAndValidateMessage = async (transaction: ITransactionObject) => {
         }
         return Promise.resolve(message);
     } catch (e) {
-        const errorMsg = `Error when processing transaction (${transaction.hash.substr(0, 8)}…)`;
+        const errorMsg = `Error when processing transaction (${transaction.hash.substr(
+            0,
+            8,
+        )}…)`;
         return Promise.reject(errorMsg);
     }
 };
@@ -87,13 +97,13 @@ function log_transaction_info(transaction: ITransactionObject) {
     const value = transaction.value;
     const issued = moment(transaction.timestamp * 1000).fromNow();
     const issuedLocale = moment(transaction.timestamp * 1000).toLocaleString();
-    const transactionInfo = ` --> Found a ${status} transaction (${startOfHash}…) for ${value} iotas` +
+    const transactionInfo =
+        ` --> Found a ${status} transaction (${startOfHash}…) for ${value} iotas` +
         `issued ${issued} [${issuedLocale}]`;
     console.log(transactionInfo);
 }
 
-const checkTransaction = (transactions: ITransactionObject[]) => (state: boolean, index: number): void => {
-    transactions[index].state = state;
+const checkTransaction = (transactions: ITransactionObject[]) => {
     transactions.filter(isValidTransaction).forEach(async (transaction) => {
         try {
             const message = await extractAndValidateMessage(transaction);
@@ -101,7 +111,10 @@ const checkTransaction = (transactions: ITransactionObject[]) => (state: boolean
                 "UPDATE acls SET rw = 1 where username = $1::text RETURNING *",
                 [message.username],
             );
-            console.log(" --> User %s successfully granted access", result.rows[0].username);
+            console.log(
+                " --> User %s successfully granted access",
+                result.rows[0].username,
+            );
         } catch (e) {
             console.log("Something went wrong when checking transaction");
             console.log(e);
@@ -113,9 +126,18 @@ const checkForPayment = async () => {
     console.log("\nChecking deposits on IOTA address %s", DEPOSIT_ADDRESS);
     try {
         const transactions = await getDeposits(DEPOSIT_ADDRESS);
-        console.log(" --> %d transactions found for address ", transactions.length, DEPOSIT_ADDRESS);
-        const states = await getLatestInclusion(transactions.map((t: any) => t.hash));
-        states.forEach(checkTransaction(transactions));
+        console.log(
+            " --> %d transactions found for address ",
+            transactions.length,
+            DEPOSIT_ADDRESS,
+        );
+        const states = await getLatestInclusion(
+            transactions.map((t: any) => t.hash),
+        );
+        states.forEach((state: boolean, index: number) => {
+            transactions[index].state = state;
+        });
+        checkTransaction(transactions);
     } catch (e) {
         console.log("Something went wrong when checking for payment");
         console.log(e);
